43eb1611960bfa598d19351431cc1bdabc2f6795
LetsCodeManh <manh.ng@icloud.com>
2022-11-16 11:05:37 +0100

    Index js standard setup

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const uuid = require("uuid");
 
 app.use(express.static("public"));
 app.use(morgan("common"));
+app.use(myLogger);
+app.use(requestTime);
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 let topBooks = [
   {
@@ -43,9 +55,6 @@ let requestTime = (req, res, next) => {
   next();
 };
 
-app.use(myLogger);
-app.use(requestTime);
-
 app.get("/", (req, res) => {
   let responseText = "Welcome to my app!";
   responseText += "<small>Requested at: " + req.requestTime + "</small>";
@@ -60,21 +69,4 @@ app.get("/secreturl", (req, res) => {
 
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
-});
-
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
 });
