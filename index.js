161cab639837767f5a3c6c6ba84b4d2f23c2470a
LetsCodeManh <manh.ng@icloud.com>
2022-12-08 19:09:35 +0100

    remove the cors

--- index.js
+++ index.js
@@ -39,27 +39,27 @@ const Director = DirectorsSchema.Director;
 const cors = require("cors");
 app.use(cors());
 
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://testsite.com",
-  "http://localhost:1234",
-];
+// let allowedOrigins = [
+//   "http://localhost:8080",
+//   "http://testsite.com",
+//   "http://localhost:1234",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        //If a specific origin isn't found on the list of allowd origins
-        let message =
-          "The CORS policy for this application doesn't allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         //If a specific origin isn't found on the list of allowd origins
+//         let message =
+//           "The CORS policy for this application doesn't allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("Welcome to this boring site! But not for long!");
